57a680b399abeb7e7ceabea2c838db961bb7ed31
HuuTri331 <nguyenhuutri331@gmail.com>
2026-08-18 15:52:23 +0700

    Conditional Operator

---
+++ js_tip/vd12/if_else.js
@@ -0,0 +1,14 @@
+const a = 10;
+// const b = null;
+
+// if(a > 6){
+//     b = true;
+// }else{
+//     b = false;
+// }
+
+// các viết điệu kiện ngắn gon hơn: 
+// const b = a > 5 ? true: false;
+
+const b = a > 11 ? "dung": "sai";
+console.log(">>> check b = ", b, "a = ", a);
